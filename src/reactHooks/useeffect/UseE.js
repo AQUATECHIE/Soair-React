ec5a1e4b3bdfa9eda10c5d6e70c763c0ae6aef88
AQUATECHIE <amolequwam96@gmail.com>
2024-08-12 18:00:43 +0100

    auto-commit

--- src/reactHooks/useeffect/UseE.js
+++ src/reactHooks/useeffect/UseE.js
@@ -4,31 +4,28 @@ import { useState, useEffect } from "react";
 
 function UseE () {
     const [count, setCount] = useState (0)
-    // const [color, setColor] = useState ('green')
-    // useEffect( () =>{
-    //     document.title = ` Count: ${count} ${color}`;
-    // }, [count, color])
+    const [color, setColor] = useState ('green')
+    useEffect( () =>{
+        document.title = ` Count: ${count} ${color}`;
+    },[count])
 
 
     const Addcount = () =>{
         setCount(count + 1)
     }
-    // const Sucount = () =>{
-    //     setCount(count - 1)
-    // }
-    // const changecolo = () =>{
-    //     setColor(count => count === 'green' ? "red " : "green")
-    // }
-
-
-
+    const Sucount = () =>{
+        setCount(count - 1)
+    }
+    const changecolo = () =>{
+        setColor(count => count === 'green' ? "red " : "green")
+    }
 
     return(
         <>
             <p style={{color: color}}> Count: {count} </p>
             <button onClick={Addcount}> Add </button>
-            {/* <button onClick={Sucount}> Minus</button>
-            <button onClick={changecolo}> Change Color</button> */}
+             <button onClick={Sucount}> Minus</button>
+            <button onClick={changecolo}> Change Color</button> 
         </>
     );
 }
